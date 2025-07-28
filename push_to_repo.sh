51d9d49c639bf64c42b80f8bb12ad4d211cc01d3
Himadri Barman <hbarhbar@gmail.com>
2025-07-29 05:10:19 +0800

    Commit on Tue 29 Jul 2025 05:10:19 CST

--- push_to_repo.sh
+++ push_to_repo.sh
@@ -1,5 +1,6 @@
 git remote add origin https://github.com/hbaromega/NonHermPhysics.git 
-git remote set-url origin https://github.com/hbaromega/NonHermPhysics.git 
+#git remote set-url origin https://github.com/hbaromega/NonHermPhysics.git 
+git remote set-url origin https://hbaromega@github.com/hbaromega/NonHermPhysics.git
 git remote -v
 git push -u origin main
 
